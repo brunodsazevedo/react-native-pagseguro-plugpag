a08d669d7df6177ecb50f7e76d6528029f5dece8
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2023-08-21 20:31:56 -0300

    feat: feito integração básica de pagamentos via POS com plugpag service

---
+++ android/src/main/java/com/pagseguroplugpag/AppException.java
@@ -0,0 +1,6 @@
+package com.pagseguroplugpag;
+
+public class AppException extends Throwable {
+  public AppException(String message) {
+  }
+}
---
+++ android/src/main/java/com/pagseguroplugpag/JsonParseUtils.java
@@ -0,0 +1,4 @@
+package com.pagseguroplugpag;
+
+public class JsonParseUtils {
+}
